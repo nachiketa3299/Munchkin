9b351d8d67a510483b89590226079b6ee3d30584
chunX2maru <netjin99@gmail.com>
2024-07-15 22:02:54 +0900

    알 내려놓기

--- Assets/Scripts/Cores/Actions/PickAction.cs
+++ Assets/Scripts/Cores/Actions/PickAction.cs
@@ -7,26 +7,36 @@ namespace MC
 	public class PickAction : MonoBehaviour
 	{
 		private bool isTriggered = false;
+		private bool isPicked = false;
 		[SerializeField] private Transform _pickPosition;
 		private GameObject pickObject;
 
 		public void BeginAction()
 		{
-			Debug.Log("Try PICK!!");
-			if (isTriggered)
+			if (!isPicked)
 			{
-				pickObject.transform.position = _pickPosition.position;
-				pickObject.transform.parent = _pickPosition;
-
-				Collider[] colliders = pickObject.GetComponents<Collider>();
-				foreach (Collider col in colliders)
+				Debug.Log("Try PICK!!");
+				if (isTriggered)
 				{
-					col.enabled = false;
-				}
+					pickObject.transform.position = _pickPosition.position;
+					pickObject.transform.parent = _pickPosition;
 
-				pickObject.GetComponent<Rigidbody>().isKinematic = true;
-				pickObject.GetComponent<Rigidbody>().useGravity = false;
-				pickObject.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
+					Collider[] colliders = pickObject.GetComponents<Collider>();
+					foreach (Collider col in colliders)
+					{
+						col.enabled = false;
+					}
+
+					pickObject.GetComponent<Rigidbody>().isKinematic = true;
+					pickObject.GetComponent<Rigidbody>().useGravity = false;
+					pickObject.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
+
+					isPicked = true;
+				}
+			}
+			else
+			{
+				DropObject();
 			}
 		}
 
@@ -42,6 +52,23 @@ namespace MC
 
 		public void EndAction()
 		{
+		}
+
+		private void DropObject()
+		{
+			Debug.Log("EndPick");
+			pickObject.transform.position = _pickPosition.position;
+			pickObject.transform.parent = null;
+			Collider[] colliders = pickObject.GetComponents<Collider>();
+			foreach (Collider col in colliders)
+			{
+				col.enabled = true;
+			}
+
+			pickObject.GetComponent<Rigidbody>().isKinematic = false;
+			pickObject.GetComponent<Rigidbody>().useGravity = true;
+			pickObject.GetComponent<Rigidbody>().interpolation = RigidbodyInterpolation.None;
+			isPicked = false;
 		}
 	}
 }
